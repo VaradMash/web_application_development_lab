fcbb34f65b74f634a399185b0e202a722e13c7c0
VaradMash <varadmash2201@gmail.com>
2022-05-06 17:56:12 +0530

    Added code for server.

---
+++ assignment_3B/index.js
@@ -0,0 +1,49 @@
+var mongoose = require("mongoose");
+var express = require("express");
+var bodyparser = require("body-parser");
+var cors = require("cors");
+var path = require("path");
+
+// Internal imports
+var routes = require("./server/routes/routes");
+
+// Connect to database
+mongoose.connect("mongodb://localhost:27017/test");
+
+// On connection
+mongoose.connection.on("connected", () => {
+    console.log("Connected to database");
+})
+
+mongoose.connection.on("error", (err) => {
+    if (err)
+    {
+        console.log("Error in database connection: " + err);
+    }
+})
+
+var app = express();
+
+// Defining the port number
+const port = 3000;
+
+// Adding middleware
+app.use(cors());
+app.use(bodyparser.json());
+
+// Static files
+app.use(express.static(path.join(__dirname, "user_profile_app")));
+
+// Connecting the routes
+app.use("/api", routes);
+
+// testing
+app.get("/", (req, res) => {
+    res.send("Hello world");
+})
+
+app.listen(port, () => {
+    console.log("Server Listening at port: " + port);
+})
+
+
