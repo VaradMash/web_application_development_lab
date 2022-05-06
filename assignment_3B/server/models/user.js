882181c2912ec571e50202d17f52c352434b03af
VaradMash <varadmash2201@gmail.com>
2022-05-06 17:57:22 +0530

    Added code for database schema.

---
+++ assignment_3B/server/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    age: {
+        type: String,
+        requires : false
+    },
+    city:{
+        type: String,
+        required: true
+    },
+    state:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+});
+
+const User = module.exports = mongoose.model("User", userSchema);
